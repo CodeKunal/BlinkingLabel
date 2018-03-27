0c5102dd58280d059906c04ff67e666bb13e3f88
Kunal Arora <kunalarora2323@gmail.com>
2018-03-27 12:31:29 +0530

    Working pod

--- Example/BlinkingLabel/ViewController.swift
+++ Example/BlinkingLabel/ViewController.swift
@@ -9,25 +9,23 @@
 import UIKit
 import BlinkingLabel
 
+
 class ViewController: UIViewController {
-    
     var isBlinking = false
-    
     let blinkingLabel = BlinkingLabel(frame: CGRect(x: 10, y: 20, width: 200, height: 30))
-    let label: BlinkingLabel
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Setup the BlinkingLabel
         blinkingLabel.text = "I blink!"
-        blinkingLabel.font = UIFont.systemFontOfSize(20)
+        blinkingLabel.font = UIFont.systemFont(ofSize: 20)
         view.addSubview(blinkingLabel)
         blinkingLabel.startBlinking()
         isBlinking = true
-        
-        // Create a UIButton to toggle the blinking
+//
+//        // Create a UIButton to toggle the blinking
         let toggleButton = UIButton(frame: CGRect(x: 10, y: 60, width: 125, height: 30))
-        
+
         toggleButton.setTitle("Toggle Blinking", for: .normal)
         toggleButton.setTitleColor(UIColor.red, for: .normal)
         toggleButton.addTarget(self, action: #selector(toggleBlinking), for: .touchUpInside)
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,5 +1,5 @@
 import XCTest
-import BlinkingLabel
+//import BlinkingLabel
 
 class Tests: XCTestCase {
     
